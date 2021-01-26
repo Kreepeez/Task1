5cc933ca6f480cfc7c5bfc7faccee4d7769fe2cb
Kreepeez <rijad.islamovic96@gmail.com>
2021-01-26 23:14:39 +0100

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.NewMapArray();
+            
+            for (int i = 0; i < test.mapArray.Length; i++)
+            {
+                test.mapArray[i].Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+            }
+
+            var result = test.GetMapById(test.mapArray, 5);
+
+            result.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+        }
+      
+    }
+}
---
+++ Task1/Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class Test
+    {
+        private Random rand;
+
+        public Dictionary<int, int>[] mapArray;
+
+        int id;
+
+        int value; 
+
+        public Test()
+        {
+            mapArray = new Dictionary<int, int>[10];
+            rand = new Random();
+        }
+
+        public Dictionary<int, int>[] NewMapArray()
+        {
+              
+            for(int i = 0; i < mapArray.Length; i++)   
+            {
+                value = rand.Next(1,10);
+                mapArray[i] = new Dictionary<int, int>() { { id, value } };
+
+                while (mapArray[i].ContainsKey(id))    
+                {
+                        id++;
+                } 
+            }
+                
+            if(mapArray != null)
+            {
+                return mapArray;
+            }
+            else Console.WriteLine("Struct is empty.");
+
+            return null;
+            
+        }
+
+        public Dictionary<int, int> GetMapById(Dictionary<int, int>[] mapArr, int id)
+        {
+            var newMap = mapArr[id];
+            return newMap;
+        }
+    }
+}
